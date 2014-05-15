be3f8b65cf0ab6ae713f55bd99a00d25fb2f5f77
Chris Drew <chris@cdrew.co.uk>
2014-05-15 01:17:57 +0100

    Continue with binary partition algorithm.

--- Rectangles/Node.cpp
+++ Rectangles/Node.cpp
@@ -5,6 +5,7 @@
 namespace {
   using rec::Node;
   using rec::Window;
+  using rec::Vec;
 
   // local helper functions
 
@@ -20,33 +21,56 @@ namespace {
     return box;
   }
 
-  //int getIntersectingCount(const Node& root, const std::list<Node>& nodes) {
-  //  int count = 0;
-  //  for (auto& node : nodes) {
-  //    if (isColision(root, getCentre(node)))
-  //      count++;
-  //  }
-  //  return count;
-  //}
+  int getIntersectingCount(const Window& box, const std::list<Window>& rectangles) {
+    int count = 0;
+    for (auto& rectangle : rectangles) {
+      if (isColision(box, getCentre(rectangle)))
+        count++;
+    }
+    return count;
+  }
 
-  //void
-  //binaryPartition(Node& node, const std::list<Node>& nodes) {
-  //  Vec second_pos;
-  //  Vec half_size;
+  void
+  divideWindow(const Window& in, Window &out1, Window &out2, float factor) {
+    Vec second_pos;
+    Vec half_size;
 
-  //  if (node.getSize().x > node.getSize().y) {
-  //    half_size = { node.getSize().x / 2, node.getSize().y };
-  //    second_pos = node.getPos() + Vec{ half_size.x, 0.0f };
-  //  } else {
-  //    half_size = { node.getSize().x, node.getSize().y / 2 };
-  //    second_pos = node.getPos() + Vec{ 0.0f, half_size.y };
-  //  }
+    if (in.size.x > in.size.y) {
+      half_size = { in.size.x * factor, in.size.y };
+      second_pos = in.pos + Vec{ half_size.x, 0.0f };
+    } else {
+      half_size = { in.size.x, in.size.y * factor };
+      second_pos = in.pos + Vec{ 0.0f, half_size.y };
+    }
 
-  //  node.setFirst(node.getPos(), half_size);
-  //  node.setSecond(second_pos, half_size);
-  //  int intersecting_count = getIntersectingCount(, nodes);
-  //  while (intersecting_count - nodes.size / 2)
-  //}
+    out1 = { in.pos, half_size };
+    out2 = { second_pos, half_size };
+  }
+
+  void
+  binaryPartition(Node& node, const std::list<Window>& rectangles) {
+    Window bounding_box = node.getWindow();
+    Window window1{ bounding_box.pos, { 0.0f, 0.0f } };
+    Window window2(bounding_box);
+    int intersecting_count = 0;
+    int half_count = int(rectangles.size() / 2);
+    int difference = -half_count;
+    float delta = 0.5f;
+    float factor = 0.0f;
+    for (int i = 0; i != 4 && std::abs(difference) > 1; ++i) {
+      if (difference < 0)
+        factor += delta;
+      else
+        factor -= delta;
+      delta /= 2.0f;
+      divideWindow(bounding_box, window1, window2, factor);
+      intersecting_count = getIntersectingCount(window1, rectangles);
+      difference = intersecting_count - half_count;
+    }
+    node.setFirst(window1);
+    node.setSecond(window2);
+    // TODO
+  }
 
 } // anonymous namespace
 
@@ -85,13 +109,14 @@ namespace rec {
 
 
   // Non-member non-friend functions
-  //Node 
-  //merge(const std::list<Window>& rectangles) {
-  //  Window root = findBoundingBox(rectangles);
-  //  binaryPartition(root, nodes);
-  //  
-  //  
+  Node 
+  merge(const std::list<Window>& rectangles) {
+    Window bounding_box = findBoundingBox(rectangles);
+    Node root(bounding_box);
+    binaryPartition(root, rectangles);
+    
+    
 
-  //  return root;
-  //}
+    return root;
+  }
 } // namespace rec
